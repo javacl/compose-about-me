c9cc3b2447e71618c20ed28d8fe87440c711ec77
sadeghi <javacl.sadeghi@gmail.com>
2022-06-18 12:34:05 +0430

    add lifecycle for flow collect

--- app/src/main/java/com/compose/navigation/features/user/ui/UserProfileScreen.kt
+++ app/src/main/java/com/compose/navigation/features/user/ui/UserProfileScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -12,7 +11,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import com.compose.navigation.core.util.rememberFlow
+import com.compose.navigation.core.util.collectAsStateLifecycleAware
 import com.compose.navigation.features.user.data.entities.UserProfileEntity
 
 @Composable
@@ -20,7 +19,7 @@ fun UserProfileScreen(
     viewModel: UserProfileViewModel,
     mainNavController: NavHostController
 ) {
-    val userProfile by rememberFlow(flow = viewModel.userProfile).collectAsState(initial = UserProfileEntity())
+    val userProfile by viewModel.userProfile.collectAsStateLifecycleAware(initial = UserProfileEntity())
     Column(
         modifier = Modifier
             .fillMaxSize()

43cc180952246a1d9ba744850fecddcf6ccd5095
sadeghi <javacl.sadeghi@gmail.com>
2022-06-28 10:28:58 +0430

    lifecycle event

--- app/src/main/java/compose/about/me/core/util/Extensions.kt
+++ app/src/main/java/compose/about/me/core/util/Extensions.kt
@@ -2,6 +2,10 @@ package compose.about.me.core.util
 
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 import kotlinx.coroutines.flow.filter
 
 @Composable
@@ -23,4 +27,19 @@ fun LazyListState.OnBottomReached(
                 onLoadMore()
             }
     }
+}
+
+@Composable
+fun rememberLifecycleEvent(lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current): Lifecycle.Event {
+    var state by remember { mutableStateOf(Lifecycle.Event.ON_ANY) }
+    DisposableEffect(lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            state = event
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+    return state
 }

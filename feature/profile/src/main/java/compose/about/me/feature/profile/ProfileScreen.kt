ffeae4c78ce56bb4bd17e1db105f6715426193a4
Javad <javacl.sadeghi@gmail.com>
2025-08-19 13:53:09 +0330

    articles

--- feature/profile/src/main/java/compose/about/me/feature/profile/ProfileScreen.kt
+++ feature/profile/src/main/java/compose/about/me/feature/profile/ProfileScreen.kt
@@ -1,7 +1,13 @@
 package compose.about.me.feature.profile
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 internal fun ProfileScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {}
 }
